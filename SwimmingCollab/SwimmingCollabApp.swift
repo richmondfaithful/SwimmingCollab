3acb831c9741d5dfa305e3dfffa407fbd545f658
Richmond Faithful <richmondfaithful@icloud.com>
2022-05-14 01:01:36 +0700

    Initial Commit

---
+++ SwimmingCollab/SwimmingCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimmingCollabApp.swift
+//  SwimmingCollab
+//
+//  Created by Richmond Faithful on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwimmingCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
