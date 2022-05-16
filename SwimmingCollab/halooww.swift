e680b37d4d18a9dcac8c871c36720dbe2738dabe
vargoalfonso515 <vargoalfonso10@gmail.com>
2022-05-16 11:37:34 +0700

    tambahin struct

--- SwimmingCollab/halooww.swift
+++ SwimmingCollab/halooww.swift
@@ -8,3 +8,18 @@
 import Foundation
 import SwiftUI
 
+struct swiperight2: View{
+    @State private var spin = false
+    var body: some View{
+        NavigationView{
+        ZStack{
+            VStack(spacing: 20){
+                HStack{
+                    Button(action: {}, label:{ Text("skip")
+                            .fontWeight(.semibold)
+                            .kerning(1.1)
+                            .foregroundColor(.black)
+                            .padding(.leading)
+                            Spacer()
+                })
+                }}}}}}
